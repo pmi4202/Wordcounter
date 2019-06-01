307f504449ccad4ff656d02d2b2715317c5dd88a
pmi4202 <pmi4202@naver.com>
2019-06-01 16:09:52 +0900

    commit-wordcounter

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\samsungpc\desktop\멋쟁이 사자처럼\vsc\wordcounter\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
